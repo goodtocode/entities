8d5954c4a3b267fdffee26c10e64d5e98f66bb83
Robert Good <robert.good@goodtocode.com>
2020-08-19 20:45:50 -0700

    EF cmd files now support connection string param

--- src/Subjects.Infrastructure.Persistence/EF-Database-Update.cmd
+++ src/Subjects.Infrastructure.Persistence/EF-Database-Update.cmd
@@ -1,4 +1,8 @@
 ﻿REM Ignore the junk char, darn bugs
+ECHO OFF
+set connectionString=%1
+IF %connectionString%.==. set connectionString="Server=tcp:goodtocodestack.database.windows.net,1433;Initial Catalog=StackData;Persist Security Info=False;User ID=LocalAdmin;Password=1202cc89-cb6f-453a-ac7e-550b3b5d2d0c;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+ECHO ON
 
 Echo ***
 Echo Update database with DbContext (changes and additions, no drops)
@@ -11,7 +15,7 @@ cd %~dp0
 dotnet tool install --global dotnet-ef
 dotnet tool update --global dotnet-ef
 
-set ConnectionStrings:MyDbConnection="Server=tcp:goodtocodestack.database.windows.net,1433;Initial Catalog=StackData;Persist Security Info=False;User ID=LocalAdmin;Password=1202cc89-cb6f-453a-ac7e-550b3b5d2d0c;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+set ConnectionStrings:MyDbConnection=%connectionString%
 
 ECHO OFF
 REM YYYYMMDD-HHMMSS
