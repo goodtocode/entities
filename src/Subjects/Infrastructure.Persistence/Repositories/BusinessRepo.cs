b00d303fc2a6dc7ce1287a52b17db82945d97b94
Robert Good <robert.good@goodtocode.com>
2023-07-03 15:07:29 -0700

    Contains working

--- src/Subjects/Infrastructure.Persistence/Repositories/BusinessRepo.cs
+++ src/Subjects/Infrastructure.Persistence/Repositories/BusinessRepo.cs
@@ -31,8 +31,7 @@ public class BusinessRepo : IBusinessRepo
     public async Task<Result<PagedResult<BusinessEntity>>> GetBusinessesByNameAsync(string businessName, int page, CancellationToken cancellationToken)
     {
         var businessResult = await _context.Business
-            //.Where(b => b.BusinessName.Contains(businessName, StringComparison.CurrentCultureIgnoreCase))
-            .Where(b => b.BusinessName == businessName)
+            .Where(b => b.BusinessName.Contains(businessName))
             .OrderBy(b => b.BusinessKey)
             .GetPagedAsync(page, _pageSize, cancellationToken);
         return Result.Success(businessResult);
