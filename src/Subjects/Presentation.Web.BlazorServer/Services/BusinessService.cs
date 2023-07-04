05b7c1bd4425409b94436424bc3ec4cc13c66e28
Robert Good <robert.good@goodtocode.com>
2023-07-04 10:51:25 -0700

    Create working

--- src/Subjects/Presentation.Web.BlazorServer/Services/BusinessService.cs
+++ src/Subjects/Presentation.Web.BlazorServer/Services/BusinessService.cs
@@ -20,7 +20,7 @@ public class BusinessService : IBusinessService
     public async Task<BusinessModel> GetBusinessAsync(Guid businessKey)
     {
         var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
-        var response = await httpClient.GetAsync($"{httpClient.BaseAddress}/Business?key={businessKey}&api-version={apiVersion}");
+        var response = await httpClient.GetAsync($"{httpClient.BaseAddress}Business?key={businessKey}&api-version={apiVersion}");
         var business = new BusinessModel();
         if (response.StatusCode != HttpStatusCode.NotFound)
         {
@@ -32,11 +32,25 @@ public class BusinessService : IBusinessService
         return business;
     }
 
+    public async Task<PagedResult<BusinessModel>> GetBusinessesAsync(int page)
+    {
+        var business = new PagedResult<BusinessModel>();
+        var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
+        var response = await httpClient.GetAsync($"{httpClient.BaseAddress}Businesses?pageNumber={page}&pageSize=20&api-version={apiVersion}");
+        if (response.StatusCode != HttpStatusCode.NotFound)
+        {
+            response.EnsureSuccessStatusCode();
+            business = JsonSerializer.Deserialize<PagedResult<BusinessModel>>(response.Content.ReadAsStream()) ?? throw new Exception("Deserialization failed.");
+        }
+
+        return business;
+    }
+
     public async Task<PagedResult<BusinessModel>> GetBusinessesAsync(string name, int page)
     {
         var business = new PagedResult<BusinessModel>();
         var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
-        var response = await httpClient.GetAsync($"{httpClient.BaseAddress}Businesses?name={name}&pageNumber=1&pageSize=20&api-version={apiVersion}");        
+        var response = await httpClient.GetAsync($"{httpClient.BaseAddress}Businesses?name={name}&pageNumber={page}&pageSize=20&api-version={apiVersion}");        
         if (response.StatusCode != HttpStatusCode.NotFound)
         {
             response.EnsureSuccessStatusCode();
@@ -49,21 +63,23 @@ public class BusinessService : IBusinessService
     public async Task CreateBusinessAsync(BusinessModel business)
     {
         var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
-        var response = await httpClient.PutAsJsonAsync<BusinessObject>($"{httpClient.BaseAddress}/Business?api-version=1", business.CopyPropertiesSafe<BusinessObject>());              
+        var businessDto = business.CopyPropertiesSafe<BusinessObject>();
+        var response = await httpClient.PutAsJsonAsync<BusinessObject>($"{httpClient.BaseAddress}Business?api-version=1", businessDto);
         response.EnsureSuccessStatusCode();
     }
 
     public async Task UpdateBusinessAsync(BusinessModel business)
     {
         var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
-        var response = await httpClient.PostAsJsonAsync<BusinessObject>($"{httpClient.BaseAddress}/Business?key={business.BusinessKey}api-version={apiVersion}", business.CopyPropertiesSafe<BusinessObject>());
+        var businessDto = business.CopyPropertiesSafe<BusinessObject>();
+        var response = await httpClient.PostAsJsonAsync<BusinessObject>($"{httpClient.BaseAddress}Business?key={business.BusinessKey}api-version={apiVersion}", businessDto);
         response.EnsureSuccessStatusCode();
     }
 
     public async Task DeleteBusinessAsync(Guid businessKey)
     {
         var httpClient = _clientFactory.CreateClient("SubjectsApiClient");
-        var response = await httpClient.DeleteAsync($"{httpClient.BaseAddress}/Business?key={businessKey}api-version={apiVersion}");
+        var response = await httpClient.DeleteAsync($"{httpClient.BaseAddress}Business?key={businessKey}api-version={apiVersion}");
         response.EnsureSuccessStatusCode();
     }
 }
